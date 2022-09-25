54bb333a4a88a02e6f80d6afe4e4058263a074cb
JotaPrimo <gestald118@gmail.com>
2022-09-24 20:47:55 -0400

    Configurando as rotas de navegação no componente principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // aqui vão ser craidas as rotas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/app.routuing.module.routing.ts
@@ -0,0 +1,7 @@
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {  },
+];
+
+export const App.routuing.moduleRoutes = RouterModule.forChild(routes);
